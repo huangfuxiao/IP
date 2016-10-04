b6f0f1550282a1f759967b644caf42c498879cf8
Bingbing Li <bli12@cs.brown.edu>
2016-10-03 21:25:41 -0400

    bli12 first commit

--- src/Driver.go
+++ src/Driver.go
@@ -3,8 +3,46 @@ package main
 import (
 	"./pkg"
 	"fmt"
+	"log"
+	"os"
 )
 
+type node struct {
+	port           int
+	interfaceArray []pkg.Interface
+	routeTable     map[string]int
+}
+
 func main() {
-	fmt.Println(pkg.Interface{1, 2})
+	//readin file
+	fileName := os.Args[1]
+
+	//test go programming
+	var interArray []pkg.Interface
+	example := pkg.Interface{Status: 1, Addr: "10.10.168.73"}
+	interArray = append(interArray, example)
+	m := make(map[string]int)
+	m["route"] = 66
+	node1 := node{port: 50001, interfaceArray: interArray, routeTable: m}
+	fmt.Println(node1.port)
+	fmt.Println(node1.interfaceArray)
+	fmt.Println(node1.routeTable)
+	fmt.Println(fileName)
+
+	//run main handler
+	file, err := os.Open("fileName") // For read access.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		b1 := make([]byte, 50)
+		n1, err2 := file.Read(b1)
+		//check(err)
+		if err2 != nil {
+			log.Fatal(err2)
+		}
+		fmt.Printf("Reading %d bytes: %s\n", n1, string(b1))
+	}
+
 }
--- src/pkg/Interface.go
+++ src/pkg/Interface.go
@@ -2,5 +2,5 @@ package pkg
 
 type Interface struct {
 	Status int
-	Addr   int
+	Addr   string
 }

8718a64b42d7f258901b8e6e855c78ce00a0c6b7
Bingbing Li <LiBing@BingbingdeMacBook-Pro.local>
2016-10-08 15:43:02 -0400

    Add Send_thread.go

--- src/runner/send_thread.go
+++ src/runner/send_thread.go
@@ -1,16 +1,32 @@
 package runner
 
 import (
+	"../handler"
 	"../ipv4"
 	"../linklayer"
 	"../pkg"
-	"fmt"
-	"strings"
 	"time"
 )
 
 func Send_thread(node *pkg.Node, u linklayer.UDPLink) {
 	for {
+		//Put all of this node.RouteTable into RIP
+		var newRip ipv4.RIP
+		newRip.Command = 2
+		newRip.NumEntries = 0
+		//put all of this node's RT's entries to RIP
+		for _, v := range node.RouteTable {
+			newRip.Entries = append(newRip.Entries, ipv4.RIPEntry{Cost: v.Cost, Address: v.Dest})
+			newRip.NumEntries++
+		}
+
+		//Loop through interfaces and send to all neighbors
+		for _, link := range node.InterfaceArray {
+			ipPkt := handler.ConvertRipToIpPackage(newRip, link.Src, srcIpAddr)
+			u.Send(ipPkt, link.RemoteAddr, link.RemotePort)
+		}
+
+		time.Sleep(5000 * time.Millisecond)
 
 	}
 }

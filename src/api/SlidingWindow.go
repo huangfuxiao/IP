0619135fa31cecf009d1b32ce14ca10026038868
ypi1 <yiwei_pi@brown.edu>
2016-11-06 18:16:59 -0500

    add helper function

--- src/api/SlidingWindow.go
+++ src/api/SlidingWindow.go
@@ -1,10 +1,17 @@
 package api
 
+import (
+	"../tcp"
+)
+
 type SendWindow struct {
 	SendBuffer      []byte
 	LastByteWritten int
 	LastByteAcked   int
 	LastByteSent    int
+	PkgInFlight     []tcp.TCPPackage
+	BytesInFlight   int
+	Size            int
 }
 
 type RecvWindow struct {
@@ -12,4 +19,27 @@ type RecvWindow struct {
 	LastByteRead     int
 	NextByteExpected int
 	LastByteRcvd     int
+	Size             int
+}
+
+func (rw *RecvWindow) AdvertisedWindow() int {
+	return size - ((rw.NextByteExpected - 1) - rw.LastByteRead)
+}
+
+func (sw *SendWindow) CheckSendAvaliable(adw int) bool {
+	if sw.LastByteSent-sw.LastByteAcked <= adw {
+		return true
+	}
+	return false
+}
+
+func (sw *SendWindow) CheckWriteAvaliable() bool {
+	if sw.LastByteWritten-sw.LastByteAcked <= sw.Size {
+		return true
+	}
+	return false
+}
+
+func (sw *SendWindow) EffectiveWindow(adw int) int {
+	return adw - sw.BytesInFlight
 }

056bcd847bae5a507e6739b138b5de761593c0fc
Bingbing Li <bli12@cs.brown.edu>
2016-11-21 21:38:56 -0500

    minor change made to recvfile_thread.go

--- src/runner/recvfile_thread.go
+++ src/runner/recvfile_thread.go
@@ -30,25 +30,26 @@ func Recvfile_thread(udp linklayer.UDPLink, thisNode *pkg.Node, mutex *sync.RWMu
 	tcb, _ := thisSocketManager.FdToSocket[socketFd]
 	newSocket := thisSocketManager.GetEstabSocket(tcb.Addr.LocalAddr, tcb.Addr.LocalPort)
 	newTcb, _ := thisSocketManager.FdToSocket[newSocket]
+	//Delete old socket
+	saddr := tcb.Addr
+	delete(thisSocketManager.FdToSocket, socketFd)
+	delete(thisSocketManager.AddrToSocket, saddr)
 
 	//Open the file to write
 	f, err := os.OpenFile(cmds[1], os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0666)
 	check(err)
-
+	fmt.Println("Start Receiving File")
 	//Keep readin and write out, until the buffer is empty
 	for {
-		fmt.Println("loop again in recvfile_thread")
 		ok, bufReadIn := thisSocketManager.V_read(newSocket, 4096, "n")
 		//time.Sleep(10 * time.Millisecond)
 		if ok != -1 {
-			fmt.Println("Read bytes ", ok)
 			//fmt.Printf("This is the string readin: %v\n", string(bufReadIn))
 			writeLines(f, bufReadIn)
 		}
-		fmt.Println("End of the loop in recvfile_thread")
 
 		if newTcb.State.State == tcp.CLOSEWAIT {
-			fmt.Println("break out")
+			fmt.Println("break")
 			break
 		}
 	}
@@ -63,33 +64,27 @@ func Recvfile_thread(udp linklayer.UDPLink, thisNode *pkg.Node, mutex *sync.RWMu
 			writeLines(f, bufReadIn)
 		}
 		if newTcb.RecvW.LastByteRead == newTcb.RecvW.NextByteExpected-1 {
+			fmt.Println("break out 2")
 			break
 		}
+		fmt.Println("loop 2")
 	}
-	f.Close()
 
-	thisSocketManager.V_shutdown(newSocket, 3)
-	for {
-		time.Sleep(1000 * time.Millisecond)
-		if newTcb.State.State == tcp.FINWAIT2 {
-			break
-		}
-	}
+	fmt.Println("End Receiving File")
+	thisSocketManager.V_close(socketFd)
+	f.Close()
+	// thisSocketManager.V_shutdown(newSocket, 3)
 
 	//Close connection at my side
-	newState, _ := tcp.StateMachine(newTcb.State.State, tcp.FIN, "")
-	newTcb.State.State = newState
+
 	// fmt.Println("This is the new state after FIN: ", newState)
-	go thisSocketManager.TimeWaitTimeOut(newTcb, 1000)
-
-	for {
-		time.Sleep(1000 * time.Millisecond)
-		if newTcb.State.State == tcp.CLOSED {
-			thisSocketManager.PrintSockets()
-			break
-		}
-
-	}
+	// for {
+	// 	time.Sleep(1000 * time.Millisecond)
+	// 	if newTcb.State.State == tcp.TIMEWAIT {
+	// 		break
+	// 	}
+	// }
+	// go thisSocketManager.TimeWaitTimeOut(newTcb, 1000)
 
 }
 
